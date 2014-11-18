06953ae810b71899764c47585f2a170fd6a29fd5
andrzej3393 <andrzej3393@gmail.com>
2014-11-18 15:52:57 +0100

    changed naming conventions

---
+++ dicks_api/__init__.py
@@ -0,0 +1,1 @@
+from .dicks_api import DicksApi
---
+++ dicks_api/dicks_api.py
@@ -0,0 +1,19 @@
+class DicksApi:
+
+    """
+    dicks-api (http://dicks-api.herokuapp.com/) client class.
+    """
+
+    def get(amount=1, size=None):
+        """
+        Get dicks from API.
+
+        Keyword arguments:
+        amount -- quantity of dicks (default 1)
+        size -- dicks size, there are 3 possible options:
+            'asian'
+            'european'
+            'black'
+            None
+        """
+        raise NotImplementedError
